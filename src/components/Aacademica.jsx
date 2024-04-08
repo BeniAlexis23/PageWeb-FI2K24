23c8ae14d16147db903e3033702cf16cf1e7cb46
BeniAlexis23 <beni_luyo2023@outlook.com>
2024-04-08 12:54:49 -0500

    correcion de texto en Aacademica

--- src/components/Aacademica.jsx
+++ src/components/Aacademica.jsx
@@ -62,7 +62,7 @@ const Aacademica = () => {
                             <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer'>
                                 <div>
                                     <img src={academica3} alt="" className='object-center object-cover h-auto w-full' />
-                                    <h5 className='text-2xl font-semibold px-5 text-center mt-5'>Reencunetro de Egresados</h5>
+                                    <h5 className='text-2xl font-semibold px-5 text-center mt-5'>Reencuentro de Egresados</h5>
                                 </div>
                             </div>
                         </motion.div>
