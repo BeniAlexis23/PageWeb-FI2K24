785ca4f38a069c25d0d97397c6dfc08ea2485b3f
BeniAlexis23 <beni_luyo2023@outlook.com>
2024-05-02 15:46:32 -0500

    add hashrouter

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Inicio from '../pages/Inicio';
 import Nosotros from '../pages/Nosotros';
 
 const Rutas = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/nosotros" element={<Nosotros />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
